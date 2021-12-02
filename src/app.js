5f652776e06c0362ba9dc4a0d4b5e2c6b4efca37
HuynhTriCo <huynhtrico@gmail.com>
2021-12-02 23:57:35 +0700

    create folders

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const { engine } = require('express-handlebars');
+const path = require('path');
+require('dotenv').config()
+//set port local
+const port = process.env.PORT || 3000;
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars')
+app.set('views', path.join(__dirname, 'views'));
+
+//As Routes are defined in pages.js
+app.use('/', require('./routes/index'));
+
+
+app.listen(port, () => {
+    console.log(`Server started on port: ${port}`);
+})
