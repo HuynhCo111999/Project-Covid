8b927762b5effc6b33813bfafe17bb17eff25aee
ngocducvo <55821615+ngocducvo@users.noreply.github.com>
2021-12-12 09:56:45 +0700

    setup-layout-with-template
    - Layout setup -- done
    - Add template --done
    - Login page --done

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 //As Routes are defined in pages.js
 app.use('/', require('./routes/index'));
-
+app.use(express.static(__dirname + '/public'));
 
 app.listen(port, () => {
     console.log(`Server started on port: ${port}`);
