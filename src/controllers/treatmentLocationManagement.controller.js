6388c15ad8b67aa46d38fbc295dd0790b6a0548a
ngocducvo <55821615+ngocducvo@users.noreply.github.com>
2021-12-19 17:18:02 +0700

    add validate input

--- src/controllers/treatmentLocationManagement.controller.js
+++ src/controllers/treatmentLocationManagement.controller.js
@@ -3,16 +3,28 @@ const treatmentLocation = db.treatmentLocation;
 
 exports.index = async (req, res) => {
     const locations = await treatmentLocation.findAll({raw : true});
-    res.render('admin/treatmentLocationManagement',{
+    locations.forEach(element => {
+        if(element.current/element.capacity < 0.9) element.color = 'success';
+        else element.color = 'danger';
+    });
+    res.render('admin/treatmentLocationManagement', {
         layout: 'admin/main',
         locations: locations,
     });
 }
 exports.add = async (req, res) => {
-    const newLocation = await treatmentLocation.create({name: req.body.name, capacity: req.body.capacity, current: req.body.current });
+    if(req.body.current > req.body.capacity) {
+        res.send('Số lượng tiếp nhận hiện tại phải nhỏ hơn sức chứa.');
+        return;
+    }
+    await treatmentLocation.create({name: req.body.name, capacity: req.body.capacity, current: req.body.current });
     res.redirect('./treatment-location');
 }
 exports.update = async (req, res) => {
+    if(req.body.current > req.body.capacity) {
+        res.send('Số lượng tiếp nhận hiện tại phải nhỏ hơn sức chứa.');
+        return;
+    }
     const id = parseInt(req.params.id);
     await treatmentLocation.update({name: req.body.name, capacity: req.body.capacity, current: req.body.current }, {
         where : {
