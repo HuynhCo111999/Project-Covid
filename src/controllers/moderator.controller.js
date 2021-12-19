261c146a67777cf4f7866d9205ca440d0d3a3817
Minh Le <lenhatminh11a1@gmail.com>
2021-12-19 21:31:28 +0700

    add user form

--- src/controllers/moderator.controller.js
+++ src/controllers/moderator.controller.js
@@ -1,3 +1,7 @@
 exports.getIndex = (req, res) => {
   res.render("moderator/main", { layout: "moderator/main" });
 };
+
+exports.getAddUser = (req, res) => {
+  res.render("moderator/add-user", { layout: "moderator/main" });
+};
--- src/routes/moderator.js
+++ src/routes/moderator.js
@@ -3,5 +3,6 @@ const moderatorController = require("../controllers/moderator.controller");
 
 // localhost:3000/moderator
 router.get("/", moderatorController.getIndex);
+router.get("/add-user", moderatorController.getAddUser);
 
 module.exports = router;
