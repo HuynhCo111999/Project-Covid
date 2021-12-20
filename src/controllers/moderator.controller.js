496c76bae538705fe0f4d03e9fd07a25bb37d2f6
Minh Le <lenhatminh11a1@gmail.com>
2021-12-20 16:50:40 +0700

    validation

--- src/controllers/moderator.controller.js
+++ src/controllers/moderator.controller.js
@@ -1,15 +1,36 @@
 const covidUser = require("../models/index").covidUser;
+const User = require("../models/index").user;
+const { validationResult } = require("express-validator/check");
 
 exports.getIndex = (req, res) => {
   res.render("moderator/main", { layout: "moderator/main" });
 };
 
 exports.getAddUser = (req, res) => {
-  res.render("moderator/add-user", { layout: "moderator/main" });
+  res.render("moderator/add-user", {
+    layout: "moderator/main",
+  });
 };
 
 exports.postAddUser = (req, res) => {
-  console.log(req.body.ward);
+  const errors = validationResult(req);
+  console.log(errors);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).render("moderator/add-user", {
+      layout: "moderator/main",
+      errorMessage: errors.array(),
+      name: req.body.name,
+      card: req.body.card,
+      yob: req.body.yob,
+      province: req.body.province,
+      district: req.body.district,
+      ward: req.body.ward,
+      status: req.body.status,
+      place: req.body.place,
+    });
+  }
+
   covidUser
     .create({
       name: req.body.name,
@@ -22,7 +43,16 @@ exports.postAddUser = (req, res) => {
       treatment_place: req.body.place,
     })
     .then(() => {
-      return res.redirect("/moderator");
+      return User.create({
+        username: req.body.card.toString(),
+        password: "12345678",
+      });
+    })
+    .then(() => {
+      res.render("moderator/add-user", {
+        layout: "moderator/main",
+        successMessage: "Thêm thành công",
+      });
     })
     .catch((err) => {
       console.log(err);
--- src/routes/moderator.js
+++ src/routes/moderator.js
@@ -1,8 +1,37 @@
 const router = require("express").Router();
 const moderatorController = require("../controllers/moderator.controller");
+const { check, body } = require("express-validator/check");
 
 // localhost:3000/moderator
 router.get("/", moderatorController.getIndex);
 router.get("/add-user", moderatorController.getAddUser);
-router.post("/add-user", moderatorController.postAddUser);
+router.post(
+  "/add-user",
+  [
+    body("name", "Họ và tên không được ít hơn 4 kí tự")
+      .trim()
+      .isLength({ min: 4 }),
+    body("card", "CMND/CCCD phải từ 9 đến 12 chữ số").isLength({
+      min: 9,
+      max: 12,
+    }),
+    body("yob", "Năm sinh không được để trống").isLength({ min: 4, max: 4 }),
+    body("province", "Tỉnh/Thành không được để trống")
+      .trim()
+      .isLength({ min: 1 }),
+    body("district", "Quận/Huyện không được để trống")
+      .trim()
+      .isLength({ min: 1 }),
+    body("ward", "Phường/Xã không được để trống").trim().isLength({ min: 1 }),
+    // body("related-person", "Tên người liên quan không được ít hơn 4 kí tự")
+    //   .trim()
+    //   .isLength({ min: 4 }),
+    // ,
+    body("place", "Nơi điều trị không được để trống")
+      .trim()
+      .isLength({ min: 1 }),
+  ],
+
+  moderatorController.postAddUser
+);
 module.exports = router;
