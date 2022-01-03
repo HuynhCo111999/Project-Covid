6b783d817a896eccb1d8b4dfb8b39236316089f3
Minh Le <lenhatminh11a1@gmail.com>
2022-01-03 14:49:50 +0700

    adding duplicated CMND/CCCD validation

--- src/controllers/moderator.controller.js
+++ src/controllers/moderator.controller.js
@@ -1,5 +1,6 @@
 const covidUser = require("../models/index").covidUser;
 const User = require("../models/index").user;
+const treatmentLocation = require("../models/index").treatmentLocation;
 const { validationResult } = require("express-validator");
 
 exports.getIndex = (req, res) => {
@@ -9,47 +10,63 @@ exports.getIndex = (req, res) => {
   });
 };
 
-exports.getAddUser = (req, res) => {
-  covidUser
-    .findAll({
-      attributes: ["name", "yob", "ward", "district", "province"],
-      raw: true,
-    })
-    .then((users) => {
-      res.render("moderator/add-user", {
-        layout: "moderator/main",
-        related_persons: users,
-        function: "add-user",
-      });
-    });
+exports.getAddUser = async (req, res) => {
+  const users = await covidUser.findAll({
+    attributes: ["name", "yob", "ward", "district", "province"],
+    raw: true,
+  });
+  const location = await treatmentLocation.findAll({
+    raw: true,
+  });
+
+  res.render("moderator/add-user", {
+    layout: "moderator/main",
+    related_persons: users,
+    location: location,
+    function: "add-user",
+  });
 };
 
-exports.postAddUser = (req, res) => {
+exports.postAddUser = async (req, res) => {
   const errors = validationResult(req);
+  const users = await covidUser.findAll({
+    raw: true,
+  });
+  const location = await treatmentLocation.findAll({
+    raw: true,
+  });
+
+  let hasUser = false;
+  for (let user of users) {
+    if (req.body.card === user.identity_card.toString()) hasUser = true;
+  }
+
+  if (hasUser) {
+    errors.errors.push({
+      value: req.body.card,
+      msg: "CMND/CCCD đã tồn tại",
+      param: "card",
+      location: "body",
+    });
+  }
 
   if (!errors.isEmpty()) {
-    return covidUser
-      .findAll({
-        attributes: ["name", "yob", "ward", "district", "province"],
-        raw: true,
-      })
-      .then((users) => {
-        res.status(422).render("moderator/add-user", {
-          layout: "moderator/main",
-          errorMessage: errors.array(),
-          related_persons: users,
-          name: req.body.name,
-          card: req.body.card,
-          yob: req.body.yob,
-          province: req.body.province,
-          district: req.body.district,
-          ward: req.body.ward,
-          status: req.body.status,
-          related_person: req.body.related_person,
-          place: req.body.place,
-          function: "add-user",
-        });
-      });
+    res.status(422).render("moderator/add-user", {
+      layout: "moderator/main",
+      errorMessage: errors.array(),
+      related_persons: users,
+      location: location,
+      name: req.body.name,
+      card: req.body.card,
+      yob: req.body.yob,
+      province: req.body.province,
+      district: req.body.district,
+      ward: req.body.ward,
+      status: req.body.status,
+      related_person: req.body.related_person,
+      place: req.body.place,
+      function: "add-user",
+    });
   }
 
   covidUser
@@ -70,20 +87,22 @@ exports.postAddUser = (req, res) => {
         password: "12345678",
       });
     })
-    .then(() => {
-      covidUser
-        .findAll({
-          attributes: ["name", "yob", "ward", "district", "province"],
-          raw: true,
-        })
-        .then((users) => {
-          res.render("moderator/add-user", {
-            layout: "moderator/main",
-            related_persons: users,
-            successMessage: "Thêm thành công",
-            function: "add-user",
-          });
-        });
+    .then(async () => {
+      const users = await covidUser.findAll({
+        attributes: ["name", "yob", "ward", "district", "province"],
+        raw: true,
+      });
+      const location = await treatmentLocation.findAll({
+        raw: true,
+      });
+
+      res.render("moderator/add-user", {
+        layout: "moderator/main",
+        related_persons: users,
+        location: location,
+        successMessage: "Thêm thành công",
+        function: "add-user",
+      });
     })
     .catch((err) => {
       console.log(err);
