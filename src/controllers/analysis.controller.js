97af31a9e75bd8e479361c7863ae192c8c93fc18
ngocducvo <55821615+ngocducvo@users.noreply.github.com>
2022-01-20 00:58:55 +0700

    add thong ke so lieu

--- src/controllers/analysis.controller.js
+++ src/controllers/analysis.controller.js
@@ -10,17 +10,6 @@ exports.index = async (req, res) => {
     var date = new Date().toISOString();
     var users;
     try {
-        // const rs = await HistoryStatus.findAll({
-        //     attributes: [[sequelize.fn('DISTINCT', sequelize.col('covidUserId')), 'alias_name'],'statusCovidUserId','createdAt'],
-        //     where: {
-        //         createdAt: {
-        //             [Op.lt]: date,
-        //         }
-        //     },
-        //     group: ['covidUserId','createdAt'],
-        //     raw: true
-        // });
-        // console.log(rs);
         users = await sequelize.query(
             `SELECT DISTINCT on ("covidUserId") "covidUserId","statusCovidUserId", "createdAt"
             FROM "histoty_user_statuses" AS "histoty_user_status" 
@@ -58,16 +47,29 @@ exports.index = async (req, res) => {
                 break;
         }
     });
+    var date2 = new Date();
+    date2.setUTCHours(0,0,0,0);
+    var text2 = date2.toISOString();
+    var numberKhoi = await getNumberByStatusToday(text2,1);
+    var numberF0 = await getNumberByStatusToday(text2,2);
+    var numberF1 = await getNumberByStatusToday(text2,3);
+    var sumKhoi = await getSumStatus(1);
+
+
     res.render("moderator/analysis", {
         layout: "moderator/main",
         function: "information-statistics",
         rs: rs,
+        numberF0: numberF0,
+        numberF1: numberF1,
+        numberKhoi: numberKhoi,
+        sumKhoi: sumKhoi,
     });
 }
 exports.getAmountByTime = async (req, res) => {
     console.log(req.body.date);
     var date = new Date(req.body.date);
-    date.setDate(date.getDate()+1);
+    date.setHours(23);
     var dateString = date.toISOString();
     // date = date.toISOString();
     // console.log(date);
@@ -112,4 +114,32 @@ exports.getAmountByTime = async (req, res) => {
         }
     });
     res.send({rs});
+}
+const getNumberByStatusToday = async (date, statusid) => {
+    try {
+        const row = await sequelize.query(
+            `SELECT DISTINCT on ("covidUserId") "covidUserId","statusCovidUserId", "createdAt"
+            FROM "histoty_user_statuses" AS "histoty_user_status" 
+            WHERE "histoty_user_status"."createdAt" > '${date}' and "statusCovidUserId" = ${statusid}
+            ORDER BY "covidUserId","statusCovidUserId", "createdAt" DESC;`
+            , { type: QueryTypes.SELECT });
+        return row.length;
+    } catch (error) {
+        console.log(error);
+    }
+    return 0;
+}
+const getSumStatus = async (statusid) => {
+    try {
+        const row = await sequelize.query(
+            `SELECT DISTINCT on ("covidUserId") "covidUserId","statusCovidUserId", "createdAt"
+            FROM "histoty_user_statuses" AS "histoty_user_status" 
+            WHERE "statusCovidUserId" = ${statusid}
+            ORDER BY "covidUserId","statusCovidUserId", "createdAt" DESC;`
+            , { type: QueryTypes.SELECT });
+        return row.length;
+    } catch (error) {
+        console.log(error);
+    }
+    return 0;
 }
