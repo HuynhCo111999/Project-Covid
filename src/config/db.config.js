80c0a9283395406ec83dfef0d889cc1cddd13e00
Long Huynh <longhuynhpyn01@gmail.com>
2022-01-20 15:40:01 +0700

    add filter price

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,37 +1,49 @@
 module.exports = {
+    //   HOST: "localhost",
+    //   USER: "postgres",
+    //   PASSWORD: "depzp",
+    //   DB: "qlcovid19",
+    //   dialect: "postgres",
+    //   pool: {
+    //     max: 5,
+    //     min: 0,
+    //     acquire: 30000,
+    //     idle: 10000
+    //   }
+    // };
+    // HOST: "localhost",
+    // USER: "postgres",
+    // PASSWORD: "123456",
+    // DB: "Covid Management",
+    // dialect: "postgres",
+    // pool: {
+    //   max: 5,
+    //   min: 0,
+    //   acquire: 30000,
+    //   idle: 10000,
+    // },
+    // };
+    // HOST: "localhost",
+    // USER: "postgres",
+    // PASSWORD: "minh15102001",
+    // DB: "Covid Management",
+    // dialect: "postgres",
+    // pool: {
+    //   max: 5,
+    //   min: 0,
+    //   acquire: 30000,
+    //   idle: 10000,
+    // },
+    // };
     HOST: "localhost",
     USER: "postgres",
-    PASSWORD: "depzp",
-    DB: "qlcovid19",
+    PASSWORD: "27121977",
+    DB: "Covid Management",
     dialect: "postgres",
     pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
-  };
-  // HOST: "localhost",
-  // USER: "postgres",
-  // PASSWORD: "123456",
-  // DB: "Covid Management",
-  // dialect: "postgres",
-  // pool: {
-  //   max: 5,
-  //   min: 0,
-  //   acquire: 30000,
-  //   idle: 10000,
-  // },
-  // };
-  // HOST: "localhost",
-  // USER: "postgres",
-  // PASSWORD: "minh15102001",
-  // DB: "Covid Management",
-  // dialect: "postgres",
-  // pool: {
-  //   max: 5,
-  //   min: 0,
-  //   acquire: 30000,
-  //   idle: 10000,
-  // },
-// };
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000,
+    },
+};
