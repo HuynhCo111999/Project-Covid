97ec495fb2ca377ca2329e0a917edb91b5246ee9
Long Huynh <longhuynhpyn01@gmail.com>
2022-01-20 10:02:29 +0700

    update UI 1.4.3

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,25 +1,38 @@
 module.exports = {
+    //   HOST: "localhost",
+    //   USER: "postgres",
+    //   PASSWORD: "082323092015",
+    //   DB: "qlcovid19",
+    //   dialect: "postgres",
+    //   pool: {
+    //     max: 5,
+    //     min: 0,
+    //     acquire: 30000,
+    //     idle: 10000
+    //   }
+    // };
+    //   HOST: "localhost",
+    //   USER: "postgres",
+    //   PASSWORD: "minh15102001",
+    //   DB: "Covid Management",
+    //   dialect: "postgres",
+    //   pool: {
+    //     max: 5,
+    //     min: 0,
+    //     acquire: 30000,
+    //     idle: 10000,
+    //   },
+    // };
+
     HOST: "localhost",
     USER: "postgres",
-    PASSWORD: "082323092015",
-    DB: "qlcovid19",
+    PASSWORD: "27121977",
+    DB: "Covid Management",
     dialect: "postgres",
     pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
-  };
-//   HOST: "localhost",
-//   USER: "postgres",
-//   PASSWORD: "minh15102001",
-//   DB: "Covid Management",
-//   dialect: "postgres",
-//   pool: {
-//     max: 5,
-//     min: 0,
-//     acquire: 30000,
-//     idle: 10000,
-//   },
-// };
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000,
+    },
+};
